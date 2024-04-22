f7ed34a795c089d51bc3e2c86c4bc838b36742c7
VietHung1 <hunghv@dolphin-technologies.net>
2024-04-22 16:00:27 +0700

    fg

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/',
-  preview: {
-    port: 5174,
-    strictPort: true,
-  },
-  server: {
-    port: 5174,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:5174",
-  },
+  base: '/testpj/',
+
 });
